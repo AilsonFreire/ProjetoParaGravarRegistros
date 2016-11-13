0609d7508e27c8ce791fc613405d961ae55324ba
Ailson Rocha Freire <ailson_freire@outlook.com>
2016-11-13 18:25:00 -0200

    v1
    Começo do projeto

---
+++ app/src/main/java/com/example/ailson/projetoparagravarregistros/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.ailson.projetoparagravarregistros;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        chamaTela1();
+    }
+
+    public void chamaTela1(){
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaTela2();
+            }
+        });
+
+    }
+
+    public void chamaTela2(){
+        setContentView(R.layout.tela2);
+
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaTela1();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaTela3();
+            }
+        });
+    }
+
+    public void chamaTela3(){
+        setContentView(R.layout.tela3);
+
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaTela2();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaTela1();
+            }
+        });
+    }
+}
