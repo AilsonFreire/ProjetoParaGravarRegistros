9ea138240cdc22ba0a510234fe94829759b42418
Ailson Rocha Freire <ailson_freire@outlook.com>
2016-11-20 22:53:18 -0200

    v1
    Gravação de registros sem sql.

--- app/src/main/java/com/example/ailson/projetoparagravarregistros/MainActivity.java
+++ app/src/main/java/com/example/ailson/projetoparagravarregistros/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.ailson.projetoparagravarregistros;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         chamaTela1();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void chamaTela1(){
+    public void chamaTela1() {
+        final Registro objRegistro;
+        final EditText editNome, editTelefone, editEmail;
+        objRegistro = new Registro();
         setContentView(R.layout.activity_main);
 
         Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+
+        editNome = (EditText) findViewById(R.id.editNome);
+        editTelefone = (EditText) findViewById(R.id.editTelefone);
+        editEmail = (EditText) findViewById(R.id.editEmail);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +51,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //gravar
+                objRegistro.nome = editNome.getText().toString();
+                objRegistro.telefone = editTelefone.getText().toString();
+                objRegistro.email = editEmail.getText().toString();
+            }
+        });
+
     }
 
-    public void chamaTela2(){
+    public void chamaTela2() {
         setContentView(R.layout.tela2);
 
         Button btn2 = (Button) findViewById(R.id.btn2);
@@ -50,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void chamaTela3(){
+    public void chamaTela3() {
         setContentView(R.layout.tela3);
 
         Button btn4 = (Button) findViewById(R.id.btn4);
@@ -69,5 +103,41 @@ public class MainActivity extends AppCompatActivity {
                 chamaTela1();
             }
         });
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/example/ailson/projetoparagravarregistros/Registro.java
@@ -0,0 +1,10 @@
+package com.example.ailson.projetoparagravarregistros;
+
+/**
+ * Created by AILSON on 20/11/2016.
+ */
+
+public class Registro {
+
+      String nome, telefone, email;
+}
